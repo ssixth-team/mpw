be7ae9c4037240918c72d77a181b44e6a7c2b254
pjs2ung <93852237+pjs2ung@users.noreply.github.com>
2026-01-28 10:47:54 +0900

    Create fab_out_asm_decision_flow.schema.ts

---
+++ src/lib/schemas/fab_out_asm_decision_flow.schema.ts
@@ -0,0 +1,15 @@
+import { type Account } from './account';
+import type { MPW_ASM_FLOW } from '$lib/schemas/asm_flow.schema';
+import type { MPW_ASM_RESULT_STATUS } from '$lib/schemas/asm_result_status.schema';
+
+export interface MPW_FAB_OUT_ASM_DECISION_FLOW {
+  id: number;
+  fabOutAsmDecisionId:number;
+  task: MPW_ASM_FLOW;
+  result: MPW_ASM_RESULT_STATUS;
+  comment: string;
+  updateUser: Account;
+  updateDate: string;
+}
+
+export const fabOutAsmDecisionFlowSchema = '++id';
